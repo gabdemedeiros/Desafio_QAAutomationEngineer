f86ebca9849c7c79c2b651f9fa4107dcc5aa19bb
gabdemedeiros <gabrielemdemedeiros@gmail.com>
2025-08-28 17:11:57 -0300

    Adição de teste de autorização de usuário

--- cypress/e2e/Desafio_Parte1.cy.js
+++ cypress/e2e/Desafio_Parte1.cy.js
@@ -29,6 +29,7 @@ context('DESAFIO QA AUTOMATION ENGONEER - PARTE 1', () => {
 
     //Gerar um token de acesso
     it('Gerar token', () => {
+
         cy.request({
             method: "POST",
             url:"https://demoqa.com/Account/v1/GenerateToken",
@@ -39,10 +40,25 @@ context('DESAFIO QA AUTOMATION ENGONEER - PARTE 1', () => {
         }).then((res) => {
             expect(res.status).to.eq(200)
             token = res.body.token
+
             cy.log("Token gerado: " + token)
+
         })
     });
     //Confirmar se o usuário criado está autorizado
+    it('Checar autorização', () => {
+        cy.request({
+            method: "POST",
+            url: "https://demoqa.com/Account/v1/Authorized",
+            body: {
+                "userName": nomeDeUsuario,
+                "password": senha
+            }
+        }).then((res) =>{
+            expect(res.status).to.eq(200)
+            expect(res.body).to.eq(true)
+        })
+    });
 
     //Listar os livros disponíveis
 
