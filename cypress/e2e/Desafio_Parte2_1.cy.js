3c6f7188822cd5d7d097a8d0d6886d93a0585ce0
gabdemedeiros <gabrielemdemedeiros@gmail.com>
2025-08-28 21:21:10 -0300

    Adição dos campos firstName, lastName, email e phone

--- cypress/e2e/Desafio_Parte2_1.cy.js
+++ cypress/e2e/Desafio_Parte2_1.cy.js
@@ -1,6 +1,14 @@
 /// <reference types="cypress" /> 
 
+import { faker } from '@faker-js/faker'
+
 context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.1', () => {
+
+    //Variáveis
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const email = faker.internet.email()
+    const phone = faker.string.numeric(10)
     it('Preenche e envia o formulário de sucesso', () => {
         
         //Acessar o site principal
@@ -13,6 +21,11 @@ context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.1', () => {
         cy.contains("Practice Form").click()
 
         //Preencher formulário
+        cy.get("#firstName").type(firstName) //Primeiro nome
+        cy.get("#lastName").type(lastName) //Sobrenome
+        cy.get("#userEmail").type(email) //E-mail
+        cy.get('label[for="gender-radio-2"]').click() //Gênero
+        cy.get("#userNumber").type(phone) //Telefone
 
         //Upload de Arquivo
 
