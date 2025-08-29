89b8afc4a45f5b072c39546b27402ca6852484e5
gabdemedeiros <gabrielemdemedeiros@gmail.com>
2025-08-28 23:01:50 -0300

    Adição de clicar no botão new Windows, janela aberta na mesma aba, validação da msg “This is a sample page e voltar para a página anterior

--- cypress/e2e/Desafio_Parte2_2.cy.js
+++ cypress/e2e/Desafio_Parte2_2.cy.js
@@ -18,8 +18,22 @@ context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.2', () => {
         //Clicar no submenu Browser Windows
         cy.contains("Browser Windows").click()
 
+        // Sobrescreve o window.open para abrir na mesma aba
+        cy.window().then(win => {
+            cy.stub(win, 'open').callsFake(url => {
+                win.location.href = url
+            })
+        })
+
         //Clicar no botão New Window
         cy.get("#windowButton").click()
+
+        // Validar a mensagem
+        cy.contains("This is a sample page").should('be.visible')
+
+        //Voltar para a aba anterior
+        cy.wait(5000) //Espera 5 segundos
+        cy.go('back')
         
     });
     
