83cc745027b956cdc50f89bb9bf11661a1235111
gabdemedeiros <gabrielemdemedeiros@gmail.com>
2025-08-29 12:16:00 -0300

    Adição dos elementos da barra de progresso do desafio 2.3

--- cypress/e2e/Desafio_Parte2_4.cy.js
+++ cypress/e2e/Desafio_Parte2_4.cy.js
@@ -4,6 +4,11 @@ context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.4', () => {
 
     it('Validar progresso do Progress Bar', () => {
 
+        // Ignora erros de scripts externos
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
+
         //Acessar o site principal
         cy.visit("https://demoqa.com/")
 
@@ -17,10 +22,27 @@ context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.4', () => {
         cy.get("#startStopButton").click()
 
         //Parar antes dos 25%
+        const pararEm25 = () => {
+        cy.get('div[role="progressbar"]').invoke('attr', 'aria-valuenow').then(v => {
+            const valor = parseInt(v)
+            if (valor >= 24) {
+            cy.get("#startStopButton").click() // Para a barra
+            } else {
+            cy.wait(10).then(pararEm25) // Espera 10ms e chama de novo
+            // O cy.wait aqui permite que a interface atualize o aria-valuenow
+            }
+        })
+        }
+        pararEm25()
 
         //Validar que o valor da progress Bar é menor ou igual aos 25%
+        cy.get('div[role="progressbar"]')
+          .invoke('attr', 'aria-valuenow')
+          .then(v => expect(parseInt(v)).to.be.lte(25))
 
         // Apertar Start novamente e ao chegar aos 100%, resetar a progress bar
+        cy.wait(5000) //Espera 5 segundos
+        cy.get("#startStopButton").click()
         
     });
     
