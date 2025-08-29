16d0506a69fe3f3e5ed005bbcf99fba047bbb09d
gabdemedeiros <gabrielemdemedeiros@gmail.com>
2025-08-29 16:14:34 -0300

    Adição do drag on drop do desafio 2.5

--- cypress/e2e/Desafio_Parte2_5.cy.js
+++ cypress/e2e/Desafio_Parte2_5.cy.js
@@ -1,8 +1,15 @@
 /// <reference types="cypress" />
 
-context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.4', () => {
+import '@4tw/cypress-drag-drop'
+
+context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.5', () => {
 
     it('Colocar elementos em ordem crescentes', () => {
+
+        // Ignora erros de scripts externos
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
 
         //Acessar o site principal
         cy.visit("https://demoqa.com/")
@@ -13,10 +20,43 @@ context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.4', () => {
         //Clicar no submenu Sortable
         cy.contains("Sortable").click()
 
-        // Reordenar a lista em ordem crescente
+        
+        // Ativar a aba List quando ela estiver visivel
+        cy.get("#demo-tab-list").click()
+        cy.get("#demo-tabpane-list").should("be.visible")
 
+        cy.get('#demo-tabpane-list').scrollIntoView()
+        cy.scrollTo('center')
+        cy.wait(5000)
+
+        // Reordenar a lista em ordem crescente
         
-        
-    });
-    
+        // O One vai para depois do Six
+        cy.contains(".list-group-item", "One")
+            .drag(".list-group-item:contains('Six')", { force: true })
+
+        // O Two vai para depois do One
+        cy.contains(".list-group-item", "Two")
+            .drag(".list-group-item:contains('One')", { force: true })
+
+        // O Three vai para depois do Two
+        cy.contains(".list-group-item", "Three")
+            .drag(".list-group-item:contains('Two')", { force: true })
+
+        // O Four vai para depois do Three
+        cy.contains(".list-group-item", "Four")
+            .drag(".list-group-item:contains('Three')", { force: true })
+
+        // O Five vai para depois do Four
+        cy.contains(".list-group-item", "Five")
+            .drag(".list-group-item:contains('Four')", { force: true })
+
+        // O Six vai para depois do Five
+        cy.contains(".list-group-item", "Six")
+            .drag(".list-group-item:contains('Five')", { force: true })
+
+        cy.get('#demo-tabpane-list').scrollIntoView()
+
+    })
+  
 });
