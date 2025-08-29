b290cc332124ce6cc39b8048242871c573ac4c51
gabdemedeiros <gabrielemdemedeiros@gmail.com>
2025-08-29 09:57:54 -0300

    Adição dos elementos do formulário no desafio 2.3

--- cypress/e2e/Desafio_Parte2_3.cy.js
+++ cypress/e2e/Desafio_Parte2_3.cy.js
@@ -1,8 +1,23 @@
 /// <reference types="cypress" />
 
+import { faker } from '@faker-js/faker'
+
 context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.3', () => {
 
+    //Variáveis do formulário
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const email = faker.internet.email()
+    const age = faker.string.numeric(2)
+    const salary = faker.string.numeric(5)
+    const department = "Tecnologia"
+
     it('Teste de tabela', () => {
+
+        // Ignora erros de scripts externos
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
 
         //Acessar o site principal
         cy.visit("https://demoqa.com/")
@@ -15,6 +30,19 @@ context('DESAFIO QA AUTOMATION ENGINEER - PARTE 2.3', () => {
 
         //Criar um novo registro
         cy.get("#addNewRecordButton").click()
+
+        //Preencher formulário
+        cy.get("#firstName").type(firstName) //Primeiro nome
+        cy.get("#lastName").type(lastName) //Sobrenome
+        cy.get("#userEmail").type(email) //E-mail
+        cy.get("#age").type(age) //Idade
+        cy.get("#salary").type(salary) //Salário
+        cy.get("#department").type("Automação") //Departamento
+
+        //Enviar formulário
+
+        cy.get("#submit").click()
+
 
         //Editar o novo registro criado
 
